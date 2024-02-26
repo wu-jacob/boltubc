e7a7a0d3647997d558f2e2af4431b3cca3d96bd5
m20arcusk <marcuskam20@gmail.com>
2024-02-25 21:47:50 -0800

    added membership button

--- src/pages/Home/components/BoltUBCTitle/BoltUBCTitle.js
+++ src/pages/Home/components/BoltUBCTitle/BoltUBCTitle.js
@@ -9,11 +9,53 @@ function BoltUBCTitle() {
     return (
 
         <div className="BoltUBCBorder" >
-            <img src={logo} className='main_dog_image'/>
+            <img src={logo} className='main_dog_image' />
             <div className='first_text_position'>
                 <div>
                     <h1 className='ubc'><h1 className='bolt'>BOLT</h1> UBC</h1>
                     <p className='catchPhrase'>Unleash the power of data analytics</p>
+                    <motion.div
+                        initial={{
+                            width: '40px',
+                            height: '35px'
+                        }}
+                        animate={{
+                            width: '250px',
+                            padding: '10px'
+                        }}
+                        // transition={{
+                        //     duration: 0.8,
+                        //     delay: 0.5,
+                        //     ease: 'backInOut',
+                        // }}
+                        style={{
+                            margin: '0 auto',
+                            borderRadius: '500px',
+                            backgroundColor: '#623CE8',
+                            translateY: '15%',
+                            color: 'white',
+                            textAlign: 'center',
+                        }}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.25 },
+                          }}
+                          whileTap={{ scale: 0.8 }}>
+                        <motion.p
+                            initial={{
+                                opacity: 0,
+                            }}
+                            animate={{
+                                opacity: 1,
+                            }}
+                            transition={{
+                                duration: 0.4,
+                                delay: 0.5,
+                                ease: 'backInOut',
+                            }}
+                            className='becomeourpartner member-button'><a href="https://docs.google.com/forms/d/e/1FAIpQLScGMtIY9xvBwmxM-29JsCiem7Mqpx6H7qAZCiVEq7i5amVK-w/viewform" style={{ color: 'white', textDecoration: 'none' }}>Become a Member!</a>
+                        </motion.p>
+                    </motion.div>
                 </div>
             </div>
         </div>
--- src/pages/Home/components/BoltUBCTitleMobile/BoltUBCTitleMobile.js
+++ src/pages/Home/components/BoltUBCTitleMobile/BoltUBCTitleMobile.js
@@ -13,6 +13,48 @@ function BoltUBCTitleMobile() {
 
             <h1 className='BoltUBCTitle'><div style={{ color: '#623CE8', display: 'inline' }}>BOLT</div> UBC</h1>
             <p className='BoltUBCCatchPhrase'>Unleash the power of data analytics</p>
+            <motion.div
+                        initial={{
+                            width: '40px',
+                            height: '35px'
+                        }}
+                        animate={{
+                            width: '250px',
+                            padding: '10px'
+                        }}
+                        // transition={{
+                        //     duration: 0.8,
+                        //     delay: 0.5,
+                        //     ease: 'backInOut',
+                        // }}
+                        style={{
+                            margin: '0 auto',
+                            borderRadius: '500px',
+                            backgroundColor: '#623CE8',
+                            translateY: '15%',
+                            color: 'white',
+                            textAlign: 'center',
+                        }}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.25 },
+                          }}
+                          whileTap={{ scale: 0.8 }}>
+                        <motion.p
+                            initial={{
+                                opacity: 0,
+                            }}
+                            animate={{
+                                opacity: 1,
+                            }}
+                            transition={{
+                                duration: 0.4,
+                                delay: 0.5,
+                                ease: 'backInOut',
+                            }}
+                            className='becomeourpartner member-button'><a href="https://docs.google.com/forms/d/e/1FAIpQLScGMtIY9xvBwmxM-29JsCiem7Mqpx6H7qAZCiVEq7i5amVK-w/viewform" style={{ color: 'white', textDecoration: 'none' }}>Become a Member!</a>
+                        </motion.p>
+                    </motion.div>
         </div>
     );
 }
